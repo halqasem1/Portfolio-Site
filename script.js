951f5f581ddd592914c4ee713b63cd6e54d93463
Hana Alqasem <121880560+halqasem1@users.noreply.github.com>
2024-05-04 20:55:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,32 +18,3 @@ links.forEach(function(l){
     })
     
 })
-
-// Responsive smooth scroll
-
-// scrollLinks.forEach(function(l){
-//     l.addEventListener("click", function(e){
-//         // prevent default
-//         e.preventDefault();
-//         // navigate to specific spot
-//         const id = e.currentTarget.getAttribute("href").slice(1);
-//         const element = document.getElementById(id);
-//         // calculate the heights
-//         const navHeight = navbar.getBoundingClientRect().height;
-//         const containerHeight = linksContainer.getBoundingClientRect().height;
-//         const fixedNav = navbar.classList.contains("fixed-nav");
-//         let position = element.offsetTop - navHeight;
-
-//         if (!fixedNav) {
-//             position = position - navHeight;
-//         }
-//         if (navHeight > 82) {
-//             position = position + containerHeight;
-//         }
-
-//         window.scrollTo({
-//             left: 0, top: position
-//         })
-//         linksContainer.style.height = 0;
-//     })
-// })
