a248aa6b7c6a359560e076aedd9214d53d60d54f
Hana Alqasem <121880560+halqasem1@users.noreply.github.com>
2024-05-20 03:57:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 
-// Smooth scroll
+// Accurate scroll for nav links
 
 let links = document.querySelectorAll(".link");
-let navbar = document.querySelector("#nav");
+let nav = document.querySelector("#nav");
+let linksContainer = document.querySelector("#linksContainer");
 
-links.forEach(function(l){
-    l.addEventListener("click", function(e){
+links.forEach(function(link) {
+    link.addEventListener("click", function(e) {
         e.preventDefault();
         const id = e.currentTarget.getAttribute("href").slice(1);
         const element = document.getElementById(id);
-        let position = element.offsetTop - 70;
-        
+        let navHeight = nav.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        let position = element.offsetTop - navHeight;
+        if (id == "home") {
+            position = 0;
+        }
         window.scrollTo({
-            left: 0, top: position,
+            left: 0,
+            top: position,
         })
+        linksContainer.style.height = 0;
     })
-    
 })
